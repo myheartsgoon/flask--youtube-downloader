a45a4f17e9a058e9305421b0718a05163ffc9573
Shi Weiwen <myheartsgoon@gmail.com>
2017-08-03 18:12:39 +0800

    Mod wkhtmltopdf path

--- convert.py
+++ convert.py
@@ -3,7 +3,8 @@ import pdfkit
 options = {
     'quiet': ''
     }
-
+path_wkthmltopdf = '/app/wkhtmltox/bin/wkhtmltopdf'
+config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
 def convert_page_to_pdf(url):
-    result = pdfkit.from_url(url, 'file/output.pdf', options=options)
+    result = pdfkit.from_url(url, 'file/output.pdf', options=options, configuration=config)
     return result
