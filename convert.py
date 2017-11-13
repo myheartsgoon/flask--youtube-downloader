8e8c6a172e264e566776d525bf474fc3a132c358
Shi <weishi@microstrategy.com>
2017-11-13 17:31:58 +0800

    fix convert

--- convert.py
+++ convert.py
@@ -3,9 +3,8 @@ import pdfkit
 options = {
     'quiet': ''
     }
-path_wkthmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+path_wkthmltopdf = '/app/wkhtmltox/bin/wkhtmltopdf'
 config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
 def convert_page_to_pdf(url):
     result = pdfkit.from_url(url, 'file/output.pdf', options=options, configuration=config)
     return result
-
